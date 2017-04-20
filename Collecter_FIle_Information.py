5516718a05bd68fea58521fea10d566fdc10dc33
pblh123 <pblh123@gmail.com>
2017-04-21 00:21:54 +0800

    Add Output Title and change time type
    Add Output Title and change time type.
    增加输出文件的中文列名，修改时间格式，方便输出文件统计。
    下一步，计划修改成直接生成excel文件

--- Collecter_FIle_Information.py
+++ Collecter_FIle_Information.py
@@ -1,18 +1,18 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'pblh123'
-#Find special file in windows
+#Find special file in windows Python3
 
 import os,sys
 import time
-import csv
+
 
 #Get create time
 def Ctime(File):
-    ctime=time.ctime(os.path.getctime(File))
+    ctime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(os.path.getctime(File)))
     return ctime
 #Last modified time
 def Mtime(File):
-    mtime=time.ctime(os.path.getmtime(File))
+    mtime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(os.path.getmtime(File)))
     return mtime
 
 #Get File size
@@ -22,13 +22,15 @@ def GetSize(File):
 
 #Get Last access time
 def Atime(File):
-    atime=time.ctime(os.path.getatime(File))
+    atime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(os.path.getatime(File)))
     return atime
 
 #Find special File of in special content
 def Find_Files_List(Path,File_Type):
     NTS=time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
     with open(Path+File_Type+'_'+NTS+'.txt','w') as F:
+        Title="文件名"+'\t'+"文件大小"+'\t'+"文件路径"+'\t'+"文件创建时间"+'\t'+"文件修改时间"+'\t'+"最后访问时间"+'\n'
+        F.write(Title)
         for root,dirs,files in os.walk(Path):
             for file in files:
                 Type='.'+File_Type
