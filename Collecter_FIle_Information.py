e711dc00710bc29ba5c1f4dd087293200af80cff
pblh123 <pblh123@gmail.com>
2017-04-30 02:15:32 +0800

    Create excel file of files
    Collect special directory files information and create a output excel file.

--- Collecter_FIle_Information.py
+++ Collecter_FIle_Information.py
@@ -25,32 +25,59 @@ def Atime(File):
     atime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(os.path.getatime(File)))
     return atime
 
-#Find special File of in special content
-def Find_Files_List(Path,File_Type):
-    NTS=time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
-    with open(Path+File_Type+'_'+NTS+'.txt','w') as F:
-        Title="文件名"+'\t'+"文件大小"+'\t'+"文件路径"+'\t'+"文件创建时间"+'\t'+"文件修改时间"+'\t'+"最后访问时间"+'\n'
-        F.write(Title)
-        for root,dirs,files in os.walk(Path):
-            for file in files:
-                Type='.'+File_Type
-                if os.path.splitext(file)[1]==Type:
-                    try:
-                        File=os.path.join(root,file)
-                        ctime=Ctime(File)
-                        mtime=Mtime(File)
-                        atime=Atime(File)
-                        fsize=GetSize(File)
-                        Str= file+'\t'+str(fsize)+'\t'+root+'\t'+ctime+'\t'+mtime+'\t'+atime+'\n'
-                        print(Str)
-                        F.write(Str)
-                    except:
-                        print('Here is wrong!')
+# create excel file
+def ToExcel(Path):
+    File=Path+'\\'+"Collecter_files.xlsx"
+    FileTypes=[]
+    for root,dirs,files in os.walk(Path):
+        for file in files:
+            if os.path.splitext(file)[1][1:] in FileTypes:
+                continue
+            else:
+                FileTypes.append(os.path.splitext(file)[1][1:])
+    print(FileTypes)
+    # create excel file
+    workbook=xlsxwriter.Workbook(File)
+    for i in FileTypes:
+        if i != '':
+            try:
+                worksheet=workbook.add_worksheet(i)
+                row=0
+                worksheet.write(row,0,"文件名")
+                worksheet.write(row,1,"文件大小")
+                worksheet.write(row,2,"文件路径")
+                worksheet.write(row,3,"文件创建时间")
+                worksheet.write(row,4,"文件修改时间")
+                worksheet.write(row,5,"最后访问时间")
+                row+=1
+            except:
+                print(i+'is unsupport forma!')
+                continue
+            for root,dirs,files in os.walk(Path):
+                for file in files:
+                    if os.path.splitext(file)[1][1:] ==i:
+                        try:
+                            File=os.path.join(root,file)
+                            ctime=Ctime(File)
+                            mtime=Mtime(File)
+                            atime=Atime(File)
+                            fsize=GetSize(File)
+                            worksheet.write(row,0,file)
+                            worksheet.write(row,1,fsize)
+                            worksheet.write(row,2,root)
+                            worksheet.write(row,3,ctime)
+                            worksheet.write(row,4,mtime)
+                            worksheet.write(row,5,atime)
+                            Str= file+'\t'+str(fsize)+'\t'+root+'\t'+ctime+'\t'+mtime+'\t'+atime+'\n'
+                            print(Str)
+                            row+=1
+                        except:
+                            print('Here is wrong!')
+
+
 
 if __name__ == '__main__':
     print('Please input path:')
     Path=input()
-    print('Please input File_Type')
-    File_Type=input()
-    Find_Files_List(Path,File_Type)
+    ToExcel(Path)
 
